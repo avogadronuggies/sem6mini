931493691103efaff8af1995aee252481d74c34b
Omaparab <omaparab004@gmail.com>
2025-04-28 19:35:37 +0530

    Final commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test_db',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
